d9f7928031ec5d5524684954e2b774c4c1b3a7ee
Mohammad-Reza Sharifi <mrsharifi76@gmail.com>
2022-03-31 16:26:24 +0430

    Add Cross Platform Compatibility

--- src/Config/repository.php
+++ src/Config/repository.php
@@ -1,21 +1,36 @@
 <?php
 
+const DR = DIRECTORY_SEPARATOR;
+
 return [
 
     'path' => [
-
-        'relative' => [
+        'namespace' => [
             'entities' => 'App\Models\Entities',
             'factories' => 'App\Models\Factories',
             'resource' => 'App\Http\Resources\Admin',
-            'repository' => 'App\Models\Repositories'
+            'repository' => 'App\Models\Repositories',
+        ],
+
+        'stubs' => [
+            'entity' => 'app'.DR.'Stubs'.DR.'Entity'.DR,
+            'factory' => 'app'.DR.'Stubs'.DR.'Factory'.DR,
+            'resource' => 'app'.DR.'Stubs'.DR.'Resource'.DR,
+            'repository' => 'app'.DR.'Stubs'.DR.'Repository'.DR,
+        ],
+
+        'relative' => [
+            'entities' => 'app'.DR.'Models'.DR.'Entities'.DR,
+            'factories' => 'app'.DR.'Models'.DR.'Factories'.DR,
+            'resource' => 'app'.DR.'Http'.DR.'Resources'.DR.'Admin'.DR,
+            'repository' => 'app'.DR.'Models'.DR.'Repositories'.DR,
         ],
 
         'absolute' => [
-            'entities' => app_path('Models/Entities'),
-            'factories' => app_path('Models/Factories'),
-            'resource' => app_path('Http/Resources/Admin'),
-            'repository' => app_path("Models/Repositories")
+            'entities' => app_path('Models'.DR.'Entities'.DR),
+            'factories' => app_path('Models'.DR.'Factories'.DR),
+            'resource' => app_path('Http'.DR.'Resources'.DR.'Admin'.DR),
+            'repository' => app_path('Models'.DR.'Repositories'.DR),
         ]
 
     ]
