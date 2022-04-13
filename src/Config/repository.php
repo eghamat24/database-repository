f33c12fb7cbcf1eff9b176a0ffdcd27b8f75c510
Mohammad-Reza Sharifi <mrsharifi76@gmail.com>
2022-04-13 11:37:35 +0430

    Revert: Change Entity Stub Path

--- src/Config/repository.php
+++ src/Config/repository.php
@@ -13,7 +13,7 @@ return [
         ],
 
         'stubs' => [
-            'entity' => __DIR__.DR.'..'.DR.'Stubs'.DR.'Entity'.DR,
+            'entity' => 'app'.'Stubs'.DR.'Entity'.DR,
             'factory' => 'app'.DR.'Stubs'.DR.'Factory'.DR,
             'resource' => 'app'.DR.'Stubs'.DR.'Resource'.DR,
             'repository' => 'app'.DR.'Stubs'.DR.'Repository'.DR,
