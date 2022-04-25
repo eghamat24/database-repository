6a74b91281f8b34349e1cf1e6c31b04519b000ac
Mohammad-Reza Sharifi <mrsharifi76@gmail.com>
2022-04-25 18:21:56 +0430

    Add Publish Functionality for Base Classes (Continued)

--- src/DatabaseRepositoryServiceProvider.php
+++ src/DatabaseRepositoryServiceProvider.php
@@ -19,6 +19,15 @@ use Illuminate\Foundation\Application as LaravelApplication;
  */
 class DatabaseRepositoryServiceProvider extends ServiceProvider
 {
+    private $baseModelStubPath;
+
+    public function __construct($app)
+    {
+        parent::__construct($app);
+
+        $this->baseModelStubPath = __DIR__ . '/../stubs/Models/PHP'.env('REPOSITORY_PHP_VERSION', '8.0');
+    }
+
     /**
      * Register the service provider.
      */
@@ -42,7 +51,64 @@ class DatabaseRepositoryServiceProvider extends ServiceProvider
             $this->publishes([
                 __DIR__ . '/../stubs/PHP'.env('REPOSITORY_PHP_VERSION', '8.0') => $this->app->basePath('stubs'),
             ], 'repository-stubs');
+
+            $this->publishEnums();
+            $this->publishEntities();
+            $this->publishFactories();
+            $this->publishResources();
+            $this->publishRepositories();
         }
+    }
+
+    private function publishEnums(): void
+    {
+        $this->publishes([
+            $this->baseModelStubPath.'/Enums/Enum.stub' => $this->app->basePath('app/Models/Enums/Enum.php'),
+        ], ['repository-base-classes', 'repository-base-enum']);
+
+        $this->publishes([
+            $this->baseModelStubPath.'/Enums/GriewFilterOperator.stub' => $this->app->basePath('app/Models/Enums/GriewFilterOperator.php'),
+        ], ['repository-base-classes', 'repository-griew-enums']);
+    }
+
+    private function publishEntities(): void
+    {
+        $this->publishes([
+            $this->baseModelStubPath.'/Entity/Entity.stub' => $this->app->basePath('app/Models/Entities/Entity.php'),
+        ], ['repository-base-classes', 'repository-base-entity']);
+    }
+
+    private function publishFactories(): void
+    {
+        $this->publishes([
+            $this->baseModelStubPath.'/Factory/Factory.stub' => $this->app->basePath('app/Models/Factories/Factory.php'),
+        ], ['repository-base-classes', 'repository-base-factory']);
+
+        $this->publishes([
+            $this->baseModelStubPath.'/Factory/IFactory.stub' => $this->app->basePath('app/Models/Factories/IFactory.php'),
+        ], ['repository-base-classes', 'repository-base-factory']);
+    }
+
+    private function publishResources(): void
+    {
+        $this->publishes([
+            $this->baseModelStubPath.'/Resource/Resource.stub' => $this->app->basePath('app/Models/Resources/Resource.php'),
+        ], ['repository-base-classes', 'repository-base-resource']);
+
+        $this->publishes([
+            $this->baseModelStubPath.'/Resource/IResource.stub' => $this->app->basePath('app/Models/Resources/IResource.php'),
+        ], ['repository-base-classes', 'repository-base-resource']);
+    }
+
+    private function publishRepositories(): void
+    {
+        $this->publishes([
+            $this->baseModelStubPath.'/Repository/MySqlRepository.stub' => $this->app->basePath('app/Models/Repositories/MySqlRepository.php'),
+        ], ['repository-base-classes', 'repository-base-mysql-repository']);
+
+        $this->publishes([
+            $this->baseModelStubPath.'/Repository/RedisRepository.stub' => $this->app->basePath('app/Models/Repositories/RedisRepository.php'),
+        ], ['repository-base-classes', 'repository-base-redis-repository']);
     }
 
     /**
